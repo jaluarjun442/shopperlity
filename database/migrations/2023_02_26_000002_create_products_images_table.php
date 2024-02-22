a8e0ec542fda534be557892d2579283d37571043
jaluarjun442 <160036840+jaluarjun442@users.noreply.github.com>
2024-02-22 23:20:59 +0530

    migration name change

--- database/migrations/2023_02_26_000002_create_products_images_table.php
+++ database/migrations/2023_02_26_000002_create_products_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductImagesTable extends Migration
+class CreateProductsImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateProductImagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('product_images', function (Blueprint $table) {
+        Schema::create('products_images', function (Blueprint $table) {
             $table->id();
             $table->integer('product_id')->nullable();
             $table->text('image')->nullable();
@@ -29,6 +29,6 @@ class CreateProductImagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product_images');
+        Schema::dropIfExists('products_images');
     }
 }
